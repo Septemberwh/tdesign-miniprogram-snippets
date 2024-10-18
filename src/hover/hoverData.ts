901554b54088b63fed39f46d4833918318d2115e
Wong <septwong@foxmail.com>
2024-10-18 19:39:20 +0800

    fix 悬停

--- src/hover/hoverData.ts
+++ src/hover/hoverData.ts
@@ -1,14 +1,79 @@
 export const hoverData = {
-  't-button': '**TDesign Button**\n\n一个常见的按钮组件，支持 \`primary\`、\`danger\` 等样式类型。',
+//   't-button': '**TDesign Button**\n\n一个常见的按钮组件，支持 \`primary\`、\`danger\` 等样式类型。',
+//   't-icon': `**TDesign Icon**\n\n
+// | 名称 | 类型 | 默认值 | 描述 | 必传 |
+// | --- | --- | --- | --- | --- |
+// | **style** | Object | - | 样式 | N |
+// | **custom-style** | Object | - | 样式，一般用于开启虚拟化组件节点场景 | N |
+// | **color** | String | - | 图标颜色 | N |
+// | **name** | String | - | 必需。图标名称或图片链接 | Y |
+// | **prefix** | String | - | 自定义图标前缀 | N |
+// | **size** | String / Number | - | 图标大小，如 \`20\`, \`20px\`, \`48rpx\`，默认单位是 \`px\` | N |
+// `,
 
-  't-icon': `**TDesign Icon**\n\n
-| 名称 | 类型 | 默认值 | 描述 | 必传 |
-| --- | --- | --- | --- | --- |
-| **style** | Object | - | 样式 | N |
-| **custom-style** | Object | - | 样式，一般用于开启虚拟化组件节点场景 | N |
-| **color** | String | - | 图标颜色 | N |
-| **name** | String | - | 必需。图标名称或图片链接 | Y |
-| **prefix** | String | - | 自定义图标前缀 | N |
-| **size** | String / Number | - | 图标大小，如 \`20\`, \`20px\`, \`48rpx\`，默认单位是 \`px\` | N |
-`,
+  't-button': '打开 [TDesign Button](https://tdesign.tencent.com/miniprogram/components/button)',
+  't-divider': '打开 [TDesign Divider](https://tdesign.tencent.com/miniprogram/components/divider)',
+  't-fab': '打开 [TDesign Fab](https://tdesign.tencent.com/miniprogram/components/fab)',
+  't-icon': '打开 [TDesign Icon](https://tdesign.tencent.com/miniprogram/components/icon)',
+  't-row': '打开 [TDesign Row](https://tdesign.tencent.com/miniprogram/components/col)',
+  't-col': '打开 [TDesign Col](https://tdesign.tencent.com/miniprogram/components/col)',
+  't-link': '打开 [TDesign Link](https://tdesign.tencent.com/miniprogram/components/link)',
+
+  't-back-top': '打开 [TDesign BackTop](https://tdesign.tencent.com/miniprogram/components/back-top)',
+  't-drawer': '打开 [TDesign Drawer](https://tdesign.tencent.com/miniprogram/components/drawer)',
+  't-indexes': '打开 [TDesign Indexes](https://tdesign.tencent.com/miniprogram/components/indexes)',
+  't-indexes-anchor': '打开 [TDesign IndexesAnchor](https://tdesign.tencent.com/miniprogram/components/indexes)',
+  't-navbar': '打开 [TDesign Navbar](https://tdesign.tencent.com/miniprogram/components/navbar)',
+  't-side-bar': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/side-bar)',
+  't-side-bar-item': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/side-bar)',
+  't-steps': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/steps)',
+  't-steps-item': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/steps)',
+  't-tab-bar': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/tab-bar)',
+  't-tab-bar-item': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/tab-bar)',
+  't-tabs': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/tabs)',
+  't-tabs-item': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/tabs)',
+
+  't-calendar': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/calendar)',
+  't-cascader': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/cascader)',
+  't-checkbox': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/checkbox)',
+  't-checkbox-group': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/checkbox)',
+  't-date-time-picker': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/date-time-picker)',
+  't-input': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/input)',
+  't-picker': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/picker)',
+  't-picker-item': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/picker)',
+  't-radio': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/radio)',
+  't-radio-group': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/radio)',
+  't-rate': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/rate)',
+  't-search': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/search)',
+  't-slider': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/slider)',
+  't-stepper': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/stepper)',
+  't-switch': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/switch)',
+  't-textarea': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/textarea)',
+  't-tree-select': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/tree-select)',
+  't-upload': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/upload)',
+
+  't-avatar': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/avatar)',
+  't-avatar-group': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/avatar)',
+  't-badge': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/badge)',
+  't-cell': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/cell)',
+  't-cell-group': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/cell)',
+  't-collapse': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/collapse)',
+  't-collapse-panel': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/collapse)',
+  't-count-down': '打开 [TDesign SideBarItem](https://tdesign.tencent.com/miniprogram/components/count-down)',
+  't-empty': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/empty)',
+  't-footer': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/footer)',
+  't-grid': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/grid)',
+  't-grid-item': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/grid)',
+  't-image': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/image)',
+  't-image-viewer': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/image-viewer)',
+  't-progress': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/progress)',
+  't-result': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/result)',
+  't-skeleton': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/skeleton)',
+  't-sticky': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/sticky)',
+  't-swiper': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/swiper)',
+  't-swiper-nav': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/swiper)',
+  't-tag': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/tag)',
+  't-check-tag': '打开 [TDesign SideBar](https://tdesign.tencent.com/miniprogram/components/tag)',
+
+  
 };
--- src/hover/index.ts
+++ src/hover/index.ts
@@ -18,9 +18,6 @@ export function getComponentNameAtPosition(
 export function getTDesignHoverContent(word: string): vscode.Hover | undefined {
   const hoverData: { [key: string]: string } = _hoverData || {};
 
-  // TODO: 悬停提示 暂时屏蔽
-  return undefined;
-
   if (hoverData[word]) {
     // return new vscode.Hover(hoverData[word]);
     const markdown = new vscode.MarkdownString(hoverData[word]);
